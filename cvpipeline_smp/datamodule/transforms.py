1f483def3b94715787ecdd0f16cb6102b9eb6152
pashkov-ai <pashkov@ai>
2026-02-15 12:38:58 -0800

    update augs

--- cvpipeline_smp/datamodule/transforms.py
+++ cvpipeline_smp/datamodule/transforms.py
@@ -4,30 +4,29 @@ from omegaconf import DictConfig
 
 def get_transforms(mode: str, cfg: DictConfig) -> list:
     transforms = [
-        A.OneOf(
-            [
-                A.RandomCrop(height=256, width=256, p=0.6),
-                A.CropNonEmptyMaskIfExists(height=256, width=256, p=1),
-            ],
-            p=1.0,
-        )
     ]
 
     if mode == "train":
         transforms.extend([
-            # A.Resize(height=cfg.datamodule.image_size.height, width=cfg.datamodule.image_size.width),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5)
-            # A.RandomBrightnessContrast(p=0.2),
-            ])
-    if mode == "validation":
-        transforms.extend([            # A.Resize(height=self.cfg.datamodule.image_size.height, width=self.cfg.datamodule.image_size.width),
             A.OneOf(
                 [
-                    A.RandomCrop(height=256, width=256, p=0.6),
+                    A.RandomCrop(height=256, width=256, p=0.05),
                     A.CropNonEmptyMaskIfExists(height=256, width=256, p=1),
                 ],
                 p=1.0,
-            )
+            ),
+            # A.Resize(height=cfg.datamodule.image_size.height, width=cfg.datamodule.image_size.width),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.3),
+            A.GaussNoise(p=0.3),
+            A.Blur(blur_limit=3, p=0.2),
+            ])
+
+    if mode == "validation":
+        transforms.extend([
+            # A.Resize(height=self.cfg.datamodule.image_size.height, width=self.cfg.datamodule.image_size.width),
+            A.RandomCrop(height=256, width=256, p=1.0)
         ])
+
     return transforms
