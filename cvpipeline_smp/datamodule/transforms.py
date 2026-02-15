ae8c9c3e8aa8ca1f2c678f0c89789db05388fe3e
pashkov-ai <pashkov@ai>
2026-02-15 13:13:45 -0800

    fix transforms

--- cvpipeline_smp/datamodule/transforms.py
+++ cvpipeline_smp/datamodule/transforms.py
@@ -18,12 +18,12 @@ def get_transforms(mode: str, cfg: DictConfig) -> list:
             # A.Resize(height=cfg.datamodule.image_size.height, width=cfg.datamodule.image_size.width),
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.3),
-            A.GaussNoise(p=0.3),
+            A.RandomBrightnessContrast((-0.1, 0.1), (-0.1, 0.1), p=0.3),
+            A.GaussNoise((0.01, 0.1), (0.0, 0.0), p=0.3),
             A.Blur(blur_limit=3, p=0.2),
             ])
 
-    if mode == "validation":
+    if mode == "valid":
         transforms.extend([
             # A.Resize(height=self.cfg.datamodule.image_size.height, width=self.cfg.datamodule.image_size.width),
             A.RandomCrop(height=256, width=256, p=1.0)
